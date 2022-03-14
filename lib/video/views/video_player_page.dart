3d1b5d212389e38c5fd5e7bdedf090e1bae9cee3
yann_mancel <yann.mancel@hotmail.fr>
2022-03-14 17:22:07 +0100

    ✨ feat: InteractiveViewer in fullscreen

--- lib/video/views/video_player_page.dart
+++ lib/video/views/video_player_page.dart
@@ -7,14 +7,19 @@ class VideoPlayerPage extends ConsumerWidget {
   const VideoPlayerPage({
     Key? key,
     this.backgroundColor = Colors.black,
+    this.hasInteractiveViewer = true,
   }) : super(key: key);
 
   final Color backgroundColor;
+  final bool hasInteractiveViewer;
 
   Future<void> _exitFullscreen(WidgetRef ref) async {
     final logic = ref.read(fullscreenLogicRef);
     await logic.exitFullscreen();
   }
+
+  double get _minScale => hasInteractiveViewer ? 0.8 : 1.0;
+  double get _maxScale => hasInteractiveViewer ? 5.0 : 1.0;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -26,12 +31,17 @@ class VideoPlayerPage extends ConsumerWidget {
       child: Material(
         color: backgroundColor,
         child: Center(
-          child: VideoPlayerWidget(
-            isFullscreen: true,
-            onTapFullscreenIcon: () async {
-              await _exitFullscreen(ref);
-              Navigator.of(context).pop<void>();
-            },
+          child: InteractiveViewer(
+            clipBehavior: Clip.none,
+            minScale: _minScale,
+            maxScale: _maxScale,
+            child: VideoPlayerWidget(
+              isFullscreen: true,
+              onTapFullscreenIcon: () async {
+                await _exitFullscreen(ref);
+                Navigator.of(context).pop<void>();
+              },
+            ),
           ),
         ),
       ),
